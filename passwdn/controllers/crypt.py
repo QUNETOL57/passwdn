014320559e7773ad7457d3924c5f180a3da5e20f
qunetol <an.1998@yandex.ru>
2019-10-25 16:45:48 +0300

    #2 create crypt controller

--- passwdn/controllers/crypt.py
+++ passwdn/controllers/crypt.py
@@ -1,4 +1,5 @@
 from cryptography.fernet import Fernet
+import os.path
 
 
 class CryptController(object):
@@ -6,17 +7,21 @@ class CryptController(object):
         self.step = 13
         self.alpha = "".join(map(chr, range(ord(' '), ord('я') + 1)))
         self.key = self.get_key()
-        self.cipher = Fernet(self.key)
+        self.cipher = Fernet(bytes(str(self.get_key()), encoding='utf-8'))
 
     def gen_key(self):
         cipher_key = Fernet.generate_key()
         file_handler = open('../local/cipher.key', 'w')
         file_handler.write(str(cipher_key)[2:-1])
         file_handler.close()
+        return str(cipher_key)[2:-1]
 
     def get_key(self):
-        file_handler = open('../local/cipher.key', 'r')
-        return bytes(file_handler.read(), encoding='utf-8')
+        if os.path.isfile('../local/cipher.key'):
+            file_handler = open('../local/cipher.key', 'r')
+            return file_handler.read()
+        else:
+            self.gen_key()
 
     def encode(self, text):
         text = bytes(text, encoding='utf-8')
@@ -32,6 +37,6 @@ class CryptController(object):
 
 
 
-# c = CryptController().gen_key()
+c = CryptController()
 # cript = c.encode('serpent26rus@gmail.com')
 # print(cript, c.decode(cript))
--- passwdn/main.py
+++ passwdn/main.py
@@ -2,21 +2,21 @@ from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
 import sys
-from models.models import ContactModel
+from models.models import LoginModel, StoreModel
 from views.views import ContactView,ListView,LoginView
 
 
 def demo(screen, scene):
     scenes = [
         Scene([LoginView(screen, contacts)], -1, name='Login'),
-        Scene([ListView(screen, contacts)], -1, name="Main"),
-        Scene([ContactView(screen, contacts)], -1, name="Edit Contact")
+        # Scene([ListView(screen, contacts)], -1, name="Main"),
+        # Scene([ContactView(screen, contacts)], -1, name="Edit Contact")
     ]
 
     screen.play(scenes, stop_on_resize=True, start_scene=scene, allow_int=True)
 
 
-contacts = ContactModel()
+contacts = LoginModel()
 last_scene = None
 while True:
     try:
