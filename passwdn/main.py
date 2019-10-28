6a24eae5b87c47c2160c298139391c44eae6f513
qunetol <an.1998@yandex.ru>
2019-10-28 10:45:51 +0300

    #2 fix main and views

--- passwdn/main.py
+++ passwdn/main.py
@@ -2,25 +2,35 @@ from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
 import sys
-from models.models import LoginModel, StoreModel
-from views.views import ContactView,ListView,LoginView
+from models import LoginModel, StoreModel
+from controllers import MainAction
+from views import ContactView,ListView,LoginView
 
 
-def demo(screen, scene):
-    scenes = [
-        Scene([LoginView(screen, contacts)], -1, name='Login'),
-        # Scene([ListView(screen, contacts)], -1, name="Main"),
-        # Scene([ContactView(screen, contacts)], -1, name="Edit Contact")
-    ]
+class PasswdnMain(object):
 
-    screen.play(scenes, stop_on_resize=True, start_scene=scene, allow_int=True)
+    def __init__(self):
+        self.l_model = LoginModel()
+        self.m_action = MainAction()
+        self.last_scene = None
+
+    def screens(self, screen, scene):
+        scenes = [
+            Scene([LoginView(screen, self.m_action, self.l_model)], -1, name='Login'),
+            # Scene([ListView(screen, contacts)], -1, name="Main"),
+            # Scene([ContactView(screen, contacts)], -1, name="Edit Contact")
+        ]
+
+        screen.play(scenes, stop_on_resize=True, start_scene=scene, allow_int=True)
+
+    def start_prog(self):
+        while True:
+            try:
+                Screen.wrapper(self.screens, catch_interrupt=True, arguments=[self.last_scene])
+                sys.exit(0)
+            except ResizeScreenError as e:
+                self.last_scene = e.scene
 
 
-contacts = LoginModel()
-last_scene = None
-while True:
-    try:
-        Screen.wrapper(demo, catch_interrupt=True, arguments=[last_scene])
-        sys.exit(0)
-    except ResizeScreenError as e:
-        last_scene = e.scene
+if __name__ == '__main__':
+    PasswdnMain().start_prog()
